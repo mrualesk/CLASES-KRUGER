4dae3ad15cc862243ef84b9747e6d0d992a19270
msruales <msruales.94@gmail.com>
2025-09-26 18:21:51 -0500

    update tarea en clase

--- FUNDAMENTOS/clase3/script.js
+++ FUNDAMENTOS/clase3/script.js
@@ -10,7 +10,7 @@ console.log("------------------")
 console.log("Inicio del bucle FOR")
 
 let sumaDesdeFor = 0;
-for( let incremental = 1; incremental <=5; incremental++) {
+for (let incremental = 1; incremental <= 5; incremental++) {
     console.log("Numero de Iteracion " + incremental)
     sumaDesdeFor = sumaDesdeFor + incremental;
 }
@@ -21,13 +21,13 @@ console.log("Fin del bucle FOR")
 let sumaDetallada = 0;
 let iteraciones = 10;
 
-for (let i = 5; i <= iteraciones; i++){
+for (let i = 5; i <= iteraciones; i++) {
     sumaDetallada = sumaDetallada + i;
     console.log(`Iteracion ${i} suma actual = ${sumaDetallada}`)
 }
 // Arrays
 // Las posiciones de los arrays | lista | arreglo siempre comienzan en 0
-           // posicion     0         1         2         3
+// posicion     0         1         2         3
 const frutas = ["Manzana", "Pera", "Platano", "Mango"];
 
 let frutaPos1 = frutas[0];
@@ -39,8 +39,8 @@ const mezcla = ["Manzana", 1, null, true];
 console.log("Tamaño del array de mezcla " + mezcla.length)
 console.log("Imprimimos la posicion 3 " + mezcla[3])
 
-for( let i = 0; i < mezcla.length; i++){
-    console.log( "Iteracion actual "+ i + " Valor " + mezcla[i])
+for (let i = 0; i < mezcla.length; i++) {
+    console.log("Iteracion actual " + i + " Valor " + mezcla[i])
 }
 
 // Hola Ana
@@ -49,7 +49,19 @@ for( let i = 0; i < mezcla.length; i++){
 // Hola Luis
 // Hola Sofía
 
+console.log("Resolucion del Ejercicio")
 
+const nombres = ["Ana", "Pedro", "Maria", "Luis", "Sofia"];
+
+for (let i = 0; i < nombres.length; i++) {
+    console.log("Hola " + nombres[i])
+}
+
+console.log("Resolucion del Ejercicio Inverso")
+
+for (let i = nombres.length - 1; i >= 0; i--) {
+    console.log("Hola " + nombres[i])
+}
 
 
 
