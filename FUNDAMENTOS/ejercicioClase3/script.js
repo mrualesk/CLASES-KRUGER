0d4b26fe3d00f3bac4c145dc0e53a3e2347297f3
msruales <msruales.94@gmail.com>
2025-10-16 20:53:02 -0500

    fundamentos ejercicios

---
+++ FUNDAMENTOS/ejercicioClase3/script.js
@@ -0,0 +1,28 @@
+// CALCULAR PROMEDIO DE NOTAS
+// Descripcion
+// Crea una función llamada promedioNotas que
+// reciba un array con las notas de un estudiante
+// y devuelva su promedio.
+
+const notas = [10, 8, 7, 10, 10];
+
+function promedioNotas(notas) {
+
+}
+
+//Crea una función llamada
+// `esPalindromo` que reciba una **palabra o frase**
+// y determine si se lee igual al derecho y al revés.
+//La función debe **ignorar los espacios
+// y mayúsculas/minúsculas**.
+
+// PISTAS
+const palindromo = "Anita lava la tina"
+const palindromoEnMinusculas = palindromo.toLowerCase();
+console.log({palindromoEnMinusculas})
+const palindromoSinEspacios = palindromoEnMinusculas.replaceAll(" ", "");
+console.log({palindromoSinEspacios})
+console.log("Primera letra del palindromo " + palindromo[0])
+
+
+
