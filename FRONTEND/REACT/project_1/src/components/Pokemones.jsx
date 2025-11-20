ba2f553b8d0fa3354b6768b213311aa10dca38cc
msruales <95254854+msruales@users.noreply.github.com>
2025-11-19 20:36:12 -0500

    Add array exercise file and expand array methods with examples in `metodos.js`.

--- FRONTEND/REACT/project_1/src/components/Pokemones.jsx
+++ FRONTEND/REACT/project_1/src/components/Pokemones.jsx
@@ -1,15 +1,59 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export function Pokemones() {
 
     const [pokemones, setPokemones] = useState([])
+    const [loading, setLoading] = useState(true)
 
-    const traerPokemons = () => {
+    const traerPokemons = async () => {
 
-        fetch("https://pokeapi.co/api/v2/pokemon?limit=150")
-            .then( )
+        // Forma antigua de ejecutar una promesa
+
+        // fetch("https://pokeapi.co/api/v2/pokemon?limit=150")
+        //     .then(data => {
+        //         // data.json() devuelve una Promesa
+        //         return data.json()
+        //     })
+        //     .then(data => {
+        //         setPokemones(data.results)
+        //     }).catch(error => {
+        //     console.log({error})
+        // }).finally(() => {
+        //     setTimeout(() => {
+        //         setLoading(false)
+        //     }, 2000)
+        //     console.log("Finalizado")
+        // })
+
+        // Forma moderna de ejecutar una promesa
+
+        try {
+            const result = await fetch("https://pokeapi.co/api/v2/pokemon?limit=150");
+            const data = await result.json();
+            setPokemones(data.results)
+            setLoading(false)
+        } catch (e) {
+            console.log(e)
+        }
+
     }
 
-    return (<>
-    </>)
+
+    useEffect(() => {
+        traerPokemons()
+    }, []);
+
+    if (loading) {
+        return <h1>Cargando...</h1>
+    }
+
+    return (
+        <>
+            <ul>
+                {
+                    pokemones.map((pokemon, index) => <li key={pokemon.name + index}>{pokemon.name}</li>)
+                }
+            </ul>
+        </>
+    )
 }
